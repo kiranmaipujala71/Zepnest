cf83a297b2adf535e4cb4e45baa3dfd4c4e6eee5
pujala Kiranmai <kiranmaipujala71@gmail.com>
2026-05-23 03:48:38 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 import './index.css'; // Import global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
